3dd8d29e6f4e7ebc2d206e1b9974815dc2eb666a
igoshev-konstantin <leader_trident@mail.ru>
2021-03-05 11:04:53 +0300

    commit 3. Подключены postgre и SpringBootApplication

---
+++ src/main/java/ru/katering/controller/MainRestController.java
@@ -0,0 +1,15 @@
+package ru.katering.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainRestController {
+
+//    @RequestMapping("/")
+//    public String getMainPage() {
+//        return "welcome";
+//    }
+
+
+}
