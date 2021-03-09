14e20239c17f661a894b402d4c1c5b44808c6764
igoshev-konstantin <leader_trident@mail.ru>
2021-03-09 16:59:46 +0300

    commit 4.

--- src/main/java/ru/katering/entity/Cook.java
+++ src/main/java/ru/katering/entity/Cook.java
@@ -12,4 +12,22 @@ public class Cook extends User{
     public void editDish(){}
     public void removeDish(){}
 
+    public Cook() {
+    }
+
+    public double getStartSalary() {
+        return startSalary;
+    }
+
+    public void setStartSalary(double startSalary) {
+        this.startSalary = startSalary;
+    }
+
+    public List<Review> getReviewList() {
+        return reviewList;
+    }
+
+    public void setReviewList(List<Review> reviewList) {
+        this.reviewList = reviewList;
+    }
 }
--- src/main/java/ru/katering/entity/User.java
+++ src/main/java/ru/katering/entity/User.java
@@ -1,10 +1,56 @@
 package ru.katering.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "User")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "login")
     private String login;
+    @Column(name = "psw")
     private String psw;
+    @Column(name = "name")
     private String name;
-    private String lastName;
 
     public void changePassword(){}
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPsw() {
+        return psw;
+    }
+
+    public void setPsw(String psw) {
+        this.psw = psw;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
